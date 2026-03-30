3f4d707507d850918f07d8cd9c4d52d1006dfc7f
viniciusbraga-dev <vinicius.braga@ges.inatel.br>
2026-03-30 19:34:39 -0300

    Add files via upload

---
+++ catalogo/js/main.js
@@ -0,0 +1,27 @@
+import { categories } from './data.js';
+import { createCarousel } from './components/Carousel.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nomePerfil = localStorage.getItem('perfilAtivoNome');
+    const imagemPerfil = localStorage.getItem('perfilAtivoImagem');
+
+    console.log('Nome do perfil:', nomePerfil);
+    console.log('Imagem do perfil:', imagemPerfil);
+
+    if (nomePerfil && imagemPerfil) {
+        const kidsLink = document.querySelector('.kids-link');
+        const profileIcon = document.querySelector('.profile-icon');
+        
+        if (kidsLink) kidsLink.textContent = nomePerfil;
+        if (profileIcon) profileIcon.src = "../" + imagemPerfil;
+    }
+
+    const container = document.getElementById('main-content');
+    
+    if (container) {
+        categories.forEach(category => {
+            const carousel = createCarousel(category);
+            container.appendChild(carousel);
+        });
+    }
+});

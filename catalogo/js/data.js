695454b0fce0877162d5a84e940546f2754512a8
viniciusbraga-dev <vinicius.braga@ges.inatel.br>
2026-03-30 21:43:34 -0300

    Add files via upload

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -1,41 +1,32 @@
 export const categories = [
     {
-        title: "Épicos",
+        title: "Melhores filmes (Minha opinão)",
         items: [
             {
-                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+                img: "https://imgs.search.brave.com/pSD80pdCzB6rlqnbNnwGBCEX1reTzwMPOhWiJlj7Tn8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9ici53/ZWIuaW1nMy5hY3N0/YS5uZXQvY18zMDBf/MzAwL21lZGlhcy9u/bWVkaWEvMTgvOTAv/MDgvNTkvMjAzNjY0/NTQuanBn", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=tivv135aGbc"
             },
             {
-                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+                img: "https://imgs.search.brave.com/fEgvNHP855iozH0Myl7dEZ_nk8c39gEm3DsWh1A7r84/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXQuY29t/L3cvZnVsbC8zLzEv/My8zMDYxMDEtMTky/MHgxMDgwLWRlc2t0/b3AtMTA4MHAtaW50/ZXJzdGVsbGFyLXdh/bGxwYXBlci1waG90/by5qcGc", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=i6avfCqKcQo"
             },
             {
                 img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
             },
             {
-                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+                img: "https://imgs.search.brave.com/3i1vWuJeVRnIqAGrZXSdTFHmqKR0ur6ZrQVJ3h1z76I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDMwNTQ3/NzIuanBn", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=5UnjrG_N8hU"
             },
         ]
     },
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://imgs.search.brave.com/V3wM9yww_fcJYos8clmbu9vUf5tXvfSp3VpKV6bM9cw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXQuY29t/L3cvZnVsbC8xLzEv/Zi8yMjg5MC0zODQw/eDIxNjAtZGVza3Rv/cC00ay1icmVha2lu/Zy1iYWQtd2FsbHBh/cGVyLXBob3RvLmpw/Zw", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=_-Nv0JOcGxg" },
+            { img: "https://imgs.search.brave.com/5bTZVrZZLrLybFpZWHFi-CrF6yEIN4_3wdAcCTN7BHs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zLmFm/aWNpb25hZG9zLmNv/bS5ici9pbWFnZW5z/L2dhbWUtb2YtdGhy/b25lcy1hcy1tZWxo/b3Jlcy1mcmFzZXMt/ZGUtdG9kb3Mtb3Mt/dGVtcG9zX3cuanBn", top10: true, youtube: "https://www.youtube.com/watch?v=KPLWWIOCOOQ" },
+            { img: "https://imgs.search.brave.com/AFRL4iAwhxOxsfevuwZCJmR5d_Swh1aBfHqfcNZZC9o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9ici53/ZWIuaW1nMy5hY3N0/YS5uZXQvY18zMDBf/MzAwL3BpY3R1cmVz/LzE4LzExLzE5LzEy/LzI2LzQ2MDQyMDUu/anBn", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=cu2ApTImBKc" },
+            { img: "https://imgs.search.brave.com/2zoTlwG-p3qOQ0k7PCVhgHO8sT3QJIuOW3yLO2gaZOQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZGlzcGxhdGUuY29t/L2FydHdvcmsvMzgw/eDI3MC8yMDIyLTAz/LTAxL2Q1YzM1ZWE5/Y2QwNTNlYjYyN2U0/ZTE2MGVjOTkxOTJh/XzAzOTAzYWZkNDY5/MmU4MzAwZjNiNTdi/MGJkMGUxNGVlLmpw/Zw", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=iS5xXr-GOnM" },
         ]
     },
-    {
-        title: "Para maratonar",
-        items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-        ]
-    }
 ];
